08b71daabfb8bc0843fb5de04b854f7b65056b54
Pierre Ugaz <ulm0@innersea.xyz>
2020-09-29 13:05:48 -0300

    fix function
    Signed-off-by: Pierre Ugaz <ulm0@innersea.xyz>

--- acls.go
+++ acls.go
@@ -81,8 +81,8 @@ func (aclClient *AclClient) GetAclsPerConsumer(consumerId string) ([]*Acl, error
 		return nil, fmt.Errorf("not authorised, message from kong: %s", body)
 	}
 
-	acls := []&Acl{}
-	err := json.Unmarshal([]byte(body), acls)
+	acls := []Acl{}
+	err := json.Unmarshal([]byte(body), &acls)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse acl get response, error: %v", err)
 	}
